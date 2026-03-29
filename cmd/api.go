5265d5a3401fa3f8815134dfe81be66932a09550
antoniobt12062002 <antoniotanahashi12@gmail.com>
2026-03-29 14:23:19 -0300

    feat: wire dependencies and register all routes

--- cmd/api.go
+++ cmd/api.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	handler "github.com/antoniobt12062002/pao-de-queijo/internal/handler/http"
 )
 
 func (app *application) mount() http.Handler {
@@ -16,11 +17,17 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("all good"))
+	})
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Post("/users", app.userHandler.Register)
+		r.Post("/auth/login", app.authHandler.Login)
+		r.Get("/auth/github", app.authHandler.GitHubLogin)
+		r.Get("/auth/github/callback", app.authHandler.GitHubCallback)
 	})
 
 	return r
@@ -35,13 +42,14 @@ func (app *application) run(h http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
-	log.Println("starting server on", app.config.addr)
-
+	slog.Info("starting server", "addr", app.config.addr)
 	return srv.ListenAndServe()
 }
 
 type application struct {
-	config config
+	config      config
+	userHandler *handler.UserHandler
+	authHandler *handler.AuthHandler
 }
 
 type config struct {
